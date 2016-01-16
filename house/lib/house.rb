1b1927aad5c71c95485592e057f44aa90fe687e8
Sandi Metz <sandi@sandimetz.com>
2016-01-16 08:18:11 -0500

    add copyright and license

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -1,14 +1,6 @@
 class House
 
-  def recite
-    1.upto(12).collect {|i| line(i)}.join("\n")
-  end
-
-  def line(num)
-    "This is #{phrase(num)}.\n"
-  end
-
-  def phrase(num)
+  DATA =
     [ "the horse and the hound and the horn that belonged to",
       "the farmer sowing his corn that kept",
       "the rooster that crowed in the morn that woke",
@@ -20,10 +12,47 @@ class House
       "the cat that killed",
       "the rat that ate",
       "the malt that lay in",
-      "the house that Jack built"].last(num).join(" ")
+      ""]
+
+  attr_reader :orderer, :data
+
+  def initialize(orderer=DefaultOrderer.new)
+    @orderer   = orderer
+  end
+
+  def recite
+    1.upto(12).collect {|i| line(i)}.join("\n")
+  end
+
+  def line(num)
+    "This is #{phrase(num)}the house that Jack built.\n"
+  end
+
+  def phrase(num)
+    orderer.order(DATA).last(num).join(" ")
+  end
+end
+
+class DefaultOrderer
+  def order(data)
+    data
+  end
+end
+
+class RandomOrderer
+  def order(data)
+    data.shuffle
+  end
+end
+
+class MostlyRandomOrderer
+  def order(data)
+    data[0...-1].shuffle + data.last
   end
 end
 
 puts
 puts House.new.line(12)
+puts
+puts House.new(RandomOrderer.new).line(12)
 puts
